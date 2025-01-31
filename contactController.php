f2bf7fd54bc8e1e762a7235e352cbe5789909b74
eldasahitii <es70008@ubt-uni.net>
2025-01-31 21:01:00 +0100

    Update contactController.php

--- contactController.php
+++ contactController.php
@@ -2,6 +2,7 @@
 session_start();
 include_once 'contactRepository.php';
 include_once 'contactModel.php';
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
