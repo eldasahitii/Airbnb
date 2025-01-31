ef0790e023c7a94739cdf01c992769006705b999
eldasahitii <es70008@ubt-uni.net>
2025-01-31 16:53:50 +0100

    edits

--- bookingRepository.php
+++ bookingRepository.php
@@ -1,20 +1,20 @@
 <?php
+require_once 'Database.php';
+require_once 'BookingModel.php';
 
 class BookingRepository {
-    private $connection;
-    private $table_name = "bookings";
+    private $conn;
 
-    public function __construct($db) {
-        $this->connection = $db;
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
     }
 
-    public function saveBooking($booking) {
-        $query = "INSERT INTO " . $this->table_name . " (apartment, name, surname, phone, email, check_in, check_out, adults, kids, special_request) 
+    public function createBooking(BookingModel $booking) {
+        $query = "INSERT INTO bookings (apartment, name, surname, phone, email, check_in, check_out, adults, kids, special_request) 
                   VALUES (:apartment, :name, :surname, :phone, :email, :check_in, :check_out, :adults, :kids, :special_request)";
         
-        $stmt = $this->connection->prepare($query);
-
-   
+        $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':apartment', $booking->getApartment());
         $stmt->bindParam(':name', $booking->getName());
         $stmt->bindParam(':surname', $booking->getSurname());
@@ -26,15 +26,12 @@ class BookingRepository {
         $stmt->bindParam(':kids', $booking->getKids());
         $stmt->bindParam(':special_request', $booking->getSpecialRequest());
 
-     
         if ($stmt->execute()) {
-            return true;
+            return $this->conn->lastInsertId();
         } else {
-            $errorInfo = $stmt->errorInfo();
-            echo "Error: " . $errorInfo[2];
             return false;
         }
     }
-}
 
+}
 ?>
